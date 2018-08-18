99c0777206d5b9a7fa678441dc53417bbde0fb28
GoriilA <601332575@qq.com>
2018-08-18 18:26:56 +0800

    vue init

---
+++ src/components/Index.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-layout row wrap justify-center align-center>
+    <v-flex xs12>
+      <v-card>
+        <v-carousel>
+          <v-carousel-item v-for="(item, index) in carouselList" :key="index" :src="item"></v-carousel-item>
+        </v-carousel>
+      </v-card>
+    </v-flex>
+    <v-flex v-for="(item, index) in bannerList" :key="index" xs6>
+      <v-parallax :src="item.src">
+        <v-layout align-center column justify-center>
+          <h1 class="slogen">{{item.slogen}}</h1>
+          <v-btn outline large color="white" class="btn-content" :to="item.to">选购{{item.type}}</v-btn>
+        </v-layout>
+      </v-parallax>
+    </v-flex>
+    <v-flex v-for="(item, index) in subBannerList" :key="index" xs4>
+      <v-parallax :src="item.src">
+        <v-layout align-center column justify-center>
+          <h1 class="slogen">{{item.slogen}}</h1>
+          <v-btn outline large color="white" class="btn-content" :to="item.to">选购{{item.type}}</v-btn>
+        </v-layout>
+      </v-parallax>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    name: "Index",
+    data() {
+      return {
+        carouselList: ['http://pbw790ert.bkt.clouddn.com/banner2.jpg', 'http://pbw790ert.bkt.clouddn.com/banner3.jpg'],
+        bannerList: [
+          {src: 'http://pbw790ert.bkt.clouddn.com/subBanner2.jpg', flex: '6', slogen: 'YEEZY SEANSON 2018', type: '鞋履', to:'sneaker'},
+          {src: 'http://pbw790ert.bkt.clouddn.com/subBanner.jpg', flex: '6', slogen: '人类制造 2018春夏系列', type: '服装', to:'clothes'}
+        ],
+        subBannerList: [
+          {src: 'http://pbw790ert.bkt.clouddn.com/subBanner4.jpg', flex: '4', slogen: 'Off White × RIMOWA', type: '箱包', to: 'bag'},
+          {src: 'http://pbw790ert.bkt.clouddn.com/subBanner5.jpg', flex: '4', slogen: 'CASIO', type: '配饰', to:'accessories'},
+          {src: 'http://pbw790ert.bkt.clouddn.com/subBanner6.jpg', flex: '4', slogen: '童年玩物系列', type: '生活小物', to:'life'}
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .slogen{
+    text-align: center;
+    font-family: "Times New Roman";
+    font-weight: inherit;
+  }
+
+</style>
---
+++ src/components/maple-main.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app id="inspire">
+    <!-- 左侧工具栏 -->
+    <v-navigation-drawer v-model="drawer" app fixed>
+      <v-subheader>商品分类</v-subheader>
+      <v-divider></v-divider>
+      <v-list dense>
+        <v-list-tile v-for="(item,index) in productTypeList" :key="index" :to="item.src">
+          <v-list-tile-content>
+            <v-list-tile-title>{{item.name}}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <!-- 顶部导航栏 -->
+    <v-toolbar color="blue darken-3" dark fixed app>
+      <v-toolbar-side-icon @click="drawer = !drawer"></v-toolbar-side-icon>
+      <router-link to="/">
+        <v-avatar size="24">
+          <img src="../assets/avatar.jpg" alt="avatar.jpg">
+        </v-avatar>
+      </router-link>
+      <v-toolbar-title>Trophy Room</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat to="login">
+          <v-icon>account_circle</v-icon>
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <!-- 中间内容 -->
+    <v-content>
+      <v-container grid-list-xl fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+    <!-- 底部 -->
+    <v-footer color="blue darken-3">
+      <v-spacer></v-spacer>
+      <p class="white--text my-4">Made By @ GorillA</p>
+      <v-spacer></v-spacer>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    name: "maple-header",
+    data() {
+      return {
+        drawer: false,
+        productTypeList: [
+          {name: '所有', src: 'all'},
+          {name: '鞋履', src: 'sneaker'},
+          {name: '服装', src: 'clothes'},
+          {name: '配饰', src: 'accessories'},
+          {name: '箱包', src: 'bag'},
+          {name: '眼镜', src: 'glasses'},
+          {name: '生活', src: 'life'}
+        ],
+        expandList: [
+          {icon: 'star', content: '收藏', src: 'baidu.com'},
+          {icon: 'bug_report', content: '发现漏洞', src: 'qq.com'}
+        ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
